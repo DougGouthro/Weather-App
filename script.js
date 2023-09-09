f32978555437dd37f22b55c0bb108657bd3fd7f8
DougGouthro <133726657+DougGouthro@users.noreply.github.com>
2023-09-09 11:54:01 -0500

    added the other days to the weather output

--- script.js
+++ script.js
@@ -6,6 +6,7 @@ var currentCity = document.querySelector("#currentCity")
 var temp = document.querySelector("#temp")
 var wind = document.querySelector("#wind")
 var humm = document.querySelector("#humm")
+var fiveDayArea = document.querySelector("#fiveDays")
 // globale variables
 // functions
 function citySearch(event) {
@@ -20,13 +21,33 @@ function citySearch(event) {
             //console.log(data.list)
             var allWeatherinfo =data.list
             var fiveDayweather = []
+            fiveDayArea.innerHTML=""
             for (var i =0; i < allWeatherinfo.length; i+=7){
                 //console.log(allWeatherinfo[i])
                 fiveDayweather.push(allWeatherinfo[i])
             }
              console.log(fiveDayweather)
-            currentCity.textContent = data.city.name
-            temp.textContent = "Temp: "+fiveDayweather[0].main.temp
+            currentCity.innerHTML = data.city.name +`<img src=" https://openweathermap.org/img/wn/${fiveDayweather[0].weather[0].icon}@2x.png" alt="Icon for the day">`
+            temp.textContent = "Temp: "+fiveDayweather
+            [0].main.temp
+            wind.textContent = "Wind:" +fiveDayweather[0].wind.speed
+            humm.textContent = "Humm:" + fiveDayweather[0].main.humidity
+            for (var i =1; i<fiveDayweather.length;i++){
+                var cardTemplet = `
+                <div class="col border border-success">
+                          <ul>
+                            <li>${fiveDayweather[i].dt_txt.split(" ")[0]}</li>
+                            <li>
+                            <img src=" https://openweathermap.org/img/wn/${fiveDayweather[i].weather[0].icon}@2x.png" alt="Icon for the day">
+                            </li>
+                            <li>Temp:${fiveDayweather[i].main.temp}</li>
+                            <li>Wind:${fiveDayweather[i].wind.speed}</li>
+                            <li>Humm:${fiveDayweather[i].main.humidity}</li>
+                          </ul>
+                        </div>
+                `
+                fiveDayArea.innerHTML+=cardTemplet
+            }
         })
 }
 // int
